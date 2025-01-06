0e39e9802851c332e075a2954b6ff8c3bf292037
sidykal <66501579+sidykal@users.noreply.github.com>
2025-01-06 13:53:53 -0800

    fix2

--- ingest.py
+++ ingest.py
@@ -3,5 +3,5 @@ import os
 os.environ["OPENAI_API_KEY"] = ''
 os.environ["LANGCHAIN_TRACING_V2"]= "true"
 os.environ["LANGCHAIN_ENDPOINT"]="https://api.smith.langchain.com"
-os.environ["LANGCHAIN_API_KEY"]="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"
+os.environ["LANGCHAIN_API_KEY"]=''
 os.environ["LANGCHAIN_PROJECT"]="pr-frosty-lime-73"
