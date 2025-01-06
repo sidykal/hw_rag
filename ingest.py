8c28246d148c038faa07f79ed5a88810b453d65d
sidykal <66501579+sidykal@users.noreply.github.com>
2025-01-06 13:42:16 -0800

    ingest file

---
+++ ingest.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ["OPENAI_API_KEY"] = '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'
+os.environ["LANGCHAIN_TRACING_V2"]= "true"
+os.environ["LANGCHAIN_ENDPOINT"]="https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"]="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"
+os.environ["LANGCHAIN_PROJECT"]="pr-frosty-lime-73"
