964034dadef58f86f687356d09bc27c0885ad963
sidykal <66501579+sidykal@users.noreply.github.com>
2025-01-09 10:48:39 -0800

    fixed ingest, added app template

---
+++ app.py
@@ -0,0 +1,51 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+from typing import cast
+
+import chainlit as cl
+import os
+
+os.environ["OPENAI_API_KEY"] = ''
+os.environ["LANGCHAIN_TRACING_V2"]= "true"
+os.environ["LANGCHAIN_ENDPOINT"]="https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"]=''
+os.environ["LANGCHAIN_PROJECT"]="pr-frosty-lime-73"
+
+llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        temperature=0,
+)
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    model = ChatOpenAI(streaming=True)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cast(Runnable, cl.user_session.get("runnable"))  # type: Runnable
+
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
--- ingest.py
+++ ingest.py
@@ -1,7 +1,34 @@
-import os
+from langchain.vectorstores import Qdrant
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
 
-#os.environ["OPENAI_API_KEY"] = ''
-os.environ["LANGCHAIN_TRACING_V2"]= "true"
-os.environ["LANGCHAIN_ENDPOINT"]="https://api.smith.langchain.com"
-os.environ["LANGCHAIN_API_KEY"]=''
-os.environ["LANGCHAIN_PROJECT"]="pr-frosty-lime-73"
+loader = PyPDFLoader("reading1.pdf")
+documents = loader.load
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 50
+)
+
+texts = text_splitter.create_documents(texts=documents)
+
+model_name = "BAAI/bge-large-en"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+
+embeddings = HuggingFaceBgeEmbeddings(
+    model_name = model_name,
+    model_kwargs = model_kwargs,
+    encode_kwargs = encode_kwargs
+)
+
+url = "http://localhost:6333"
+
+qdrant = Qdrant.from_documents(
+    texts,
+    embeddings,
+    url=url,
+    prefer_grpc=False,
+    collection_name="my_documents",
+)
